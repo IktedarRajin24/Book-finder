025405f56570d1282ea689e78676aa8faf1663e8
MD. Iktedar Hasan Rushdi <rushdi2410@gmail.com>
2024-03-13 03:27:18 +0600

    Header and footer done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./Layouts/Footer";
+import Header from "./Layouts/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
